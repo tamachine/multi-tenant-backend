2742417b141237cd94c7e10ab6767f87a4538fbd
tamara.rios@scandinavianehf.com <tamara.rios@scandinavianehf.com>
2022-10-28 14:39:39 +0200

    [FEAT] add w-fill-sreen class

--- resources/views/components/home/hero.blade.php
+++ resources/views/components/home/hero.blade.php
@@ -1,4 +1,4 @@
-<div class="text-pink-red bg-home-hero h-[718px] bg-cover bg-bottom bg-no-repeat ">
+<div class="text-pink-red bg-home-hero h-[718px] bg-cover bg-center bg-no-repeat w-fill-screen">
    <h1 class="pt-12">{{ __('web.home.title') }}</h1>
    <h3 class="pt-3">{{ __('web.home.subtitle') }}</h3>
 </div>
