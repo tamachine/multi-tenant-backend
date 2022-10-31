0613913e411c882b1caf4eaca3ed81b9fada1b82
tamara.rios@scandinavianehf.com <tamara.rios@scandinavianehf.com>
2022-10-31 09:59:35 +0100

    [FEAT] add box 2 heading component in home

--- resources/views/web/home/index.blade.php
+++ resources/views/web/home/index.blade.php
@@ -19,7 +19,15 @@
         </div>
 
         @include('web.home.cards-default')
-               
+
+        <div class="pt-36 pb-12">
+            <x-heading-h2 
+                title="{{ __('web.home.box2-h2-title') }}" 
+                subtitle="{{ __('web.home.box2-h2-subtitle') }}" 
+                tailwind-align-class="text-left"
+                tailwind-padding-class="pt-2"
+            />
+        </div>
     </div>
     
 @endsection
