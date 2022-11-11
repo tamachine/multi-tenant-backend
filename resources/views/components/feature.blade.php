a7486234e2a5b1199faf8d175ac4c0ff965f414f
tamara.rios@scandinavianehf.com <tamara.rios@scandinavianehf.com>
2022-11-11 10:58:32 +0100

    [FEAT] add missing files

--- resources/views/components/feature.blade.php
+++ resources/views/components/feature.blade.php
@@ -1,8 +1,13 @@
 <div class="flex flex-col md:{{ $tailwindDesktopDirectionClass }}">
+    
     <div 
-        class="md:flex-1 rounded-2xl bg-cover {{ $paddingCol2 }}" 
-        style="background-image: url('{{ $imagePath }}')">
+        class="hidden md:block md:flex-1 md:rounded-2xl md:bg-cover {{ $paddingCol2 }}" 
+        style="background-image: url('{{ $imagePath }}')">        
     </div>
+    <div class="md:hidden {{ $paddingCol2 }} pb-5">        
+        <img class="rounded-2xl bg-cover w-full" src="{{ $imagePath }}" />
+    </div>
+
     <div class="md:flex-1 flex flex-col md:{{ $itemsClass }} gap-7">
         <h2 class="md:{{ $textAlign }} {{ $paddingCol1 }}">{!! $title !!}</h2>
         <div class="{{ $paddingCol1 }}">
