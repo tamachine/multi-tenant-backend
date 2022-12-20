4bc9f1ea495ac13a084f92f28aaca5bbd08d9110
tamara.rios@scandinavianehf.com <tamara.rios@scandinavianehf.com>
2022-12-20 13:14:57 +0100

    cars title and subtitle

---
+++ database/seeders/Translations/Web/CarsSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders\Translations\Web;
+
+use Illuminate\Database\Seeder;
+use App\Models\Translation;
+
+class CarsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {                
+        Translation::firstOrCreate(
+            [
+                'group' => 'cars',
+                'key' => 'title',    
+            ],
+            [                
+                'text' => ['en' => 'Browse through all of our available vehicles', 'es' => 'Navega a través de todos nuestros vehículos disponibles'],
+            ]
+        );   
+
+        Translation::firstOrCreate(
+            [
+                'group' => 'cars',
+                'key' => 'subtitle',    
+            ],
+            [                
+                'text' => ['en' => 'The fleet guide below provides an indication of the size and style of car available for each car group. <br>Please note that reservations can only be made by car group and are subject to availability.', 'es' => 'La siguiente guía de flotas proporciona una indicación del tamaño y estilo del automóvil disponible para cada grupo de automóviles. <br>Tenga en cuenta que las reservas solo se pueden hacer por grupo de vehículos y están sujetas a disponibilidad.'],
+                'rich' => true,
+            ]
+        );   
+        
+    }
+}
--- resources/views/web/cars/index.blade.php
+++ resources/views/web/cars/index.blade.php
@@ -3,7 +3,12 @@
 @section('body')        
     <div class="bg-[#FCFCFC] w-fill-screen">
         <div class="max-w-7xl mx-auto ">
+            
             <x-breadcrumb :breadcrumbs="$breadcrumbs" />
+
+            <div class="hidden md:block">
+                <x-heading-h1 title="{!! __('cars.title') !!}" text="{!! __('cars.subtitle') !!}" :small-text="true"/>            
+            </div>
         </div>
     </div>
 @endsection
