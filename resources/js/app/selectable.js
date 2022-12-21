1377cc3a0560cf75a30148f4721ff2fb6e854323
tamara.rios@scandinavianehf.com <tamara.rios@scandinavianehf.com>
2022-12-21 10:49:07 +0100

    move selectable javascript to js file

--- resources/js/app/selectable.js
+++ resources/js/app/selectable.js
@@ -58,34 +58,3 @@ selectable = function(config) {
         },
     }
 }  
-selectOption = function (config) {
-    return {
-        selectedOption: config.selectedOption,
-
-        select(option) {
-            this.selectedOption = option
-        },                
-    };
-}
-
-visibilitySelector = function () {
-    return {
-        show: false,
-
-        close: function() {
-            this.show = false
-        },
-
-        open: function() {
-            this.show = true
-        },
-
-        toggle: function() {
-            this.show = !this.show
-        },
-        
-        visibility: function() {
-            return this.show
-        }
-    };
-}
