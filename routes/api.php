d33275d92aedf6607d49882ff1c644bb14ada78f
tamara.rios@scandinavianehf.com <tamara.rios@scandinavianehf.com>
2023-01-04 10:49:17 +0100

    create token info

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/tokens/create', function (Request $request) {
-    $token = User::where('name','api')->first()->createToken('front-end-1');
-     
-    echo $token->plainTextToken;    
+    $token = User::where('name', 'api')->first()->createToken('front-end-1');
+    
+    echo '<b>token</b>: '.$token->plainTextToken.'<br>';
+    echo '<b>get</b>: '. '/api/user<br>';
+    echo '<b>headers</b>: <br>';
+    echo 'Accept: application/json: <br>';
+    echo 'Bearer '.$token->plainTextToken.'<br>';
+
+    die; 
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
