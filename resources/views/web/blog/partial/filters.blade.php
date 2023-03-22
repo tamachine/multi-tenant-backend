6aba62953d297c7dc682485abad28a759cbc84a4
tamar <tamar@TAMARA.local>
2023-03-22 09:44:58 +0100

    feat: blog translations

--- resources/views/web/blog/partial/filters.blade.php
+++ resources/views/web/blog/partial/filters.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-col gap-3 mb-20 mt-14 md:mb-14 md:mt-10">
-    <div class="font-fredoka-semibold text-lg">View by category</div>
+    <div class="font-fredoka-semibold text-lg">{!! __('blog.view-by-category') !!}</div>
     <div class="flex flex-col md:flex-row md:justify-between md:items-center gap-7 md:gap-[10px]">
         @include('web.blog.partial.categories')
     
