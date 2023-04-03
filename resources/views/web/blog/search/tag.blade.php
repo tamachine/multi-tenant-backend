b1849d02cbe6ef51224a8c203c7f424f8d0f7f2d
tamar <tamar@TAMARA.local>
2023-04-03 10:40:40 +0200

    refactor: remove blogTag unnecessary files

--- resources/views/web/blog/search/tag.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.web')
-
-@section('body')    
-    <div class="px-3 md:px-0 max-w-6xl mx-auto">
-        <livewire:web.blog-search-tag :blogTag="$tag" />                       
-    </div>
-@endsection
