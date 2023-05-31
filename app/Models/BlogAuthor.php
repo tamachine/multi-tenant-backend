9aa5a7c14f7b0e7ebe4d15e5de3953f465f2e3db
tamar <tamar@TAMARA.local>
2023-05-31 10:31:19 +0200

    feature: add seoConfigurations to blog author

--- app/Models/BlogAuthor.php
+++ app/Models/BlogAuthor.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Traits\HasFeaturedImage;
+use App\Traits\HasSEOConfigurations;
 use \Mcamara\LaravelLocalization\Interfaces\LocalizedUrlRoutable;
 
 class BlogAuthor extends Model implements LocalizedUrlRoutable
 {
-    use HasFactory, HashidTrait, SoftDeletes, HasFeaturedImage;
+    use HasFactory, HashidTrait, SoftDeletes, HasFeaturedImage, HasSEOConfigurations;
 
     //for HasFeaturedImage
     protected $featured_image_attribute = 'photo';
