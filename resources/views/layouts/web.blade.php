810f784712d6105fb95c76173817b6cd66dfcd33
Ester Martínez <ester@scandinavianehf.com>
2023-06-13 11:37:09 +0200

    Interfaz navegador rosa

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -7,6 +7,7 @@
     >
     <head>
         <meta charset="utf-8">
+        <meta name="theme-color" content="#E11166" >
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <x-seo-tags />
