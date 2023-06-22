c2ac0035bcbbccdfb7618d0272fec1bb25dc6a1c
tamar <tamar@TAMARA.local>
2023-06-22 15:36:51 +0200

    fix: remove coment from SuccessController

--- app/Http/Controllers/Web/SuccessController.php
+++ app/Http/Controllers/Web/SuccessController.php
@@ -6,7 +6,6 @@ use Valitor;
 use App\Jobs\CreateBookingPdf;
 use App\Models\Booking;
 
-//http://nave-intergalactica.test/success?AuthorizationNumber=323575&CardNumberMasked=999999%2A%2A%2A%2A%2A%2A9999&CardType=%C3%93%C3%9EEKKT%20KORTATEGUND%3A%20RUPAY&CardTypeCode=&ContractNumber=800573&ContractType=ORUGGS&Date=22.06.2023&DigitalSignatureResponse=00470dba4076af958bb3628e0c905f38f5369c3039f95f8a4896a79684945210&ReferenceNumber=JQ1J4OF45O&SaleID=f815b100-129b-4ce9-bea6-bfa798b84c97&TransactionNumber=317310667401git pu
 class SuccessController extends BaseController
 {
     protected $booking;    
