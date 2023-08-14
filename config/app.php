0d7d5b42ae0d68abc17108657d256211d546cdaa
tamar <tamar@TAMARA.local>
2023-08-14 12:42:00 +0200

    refactor: create bindings service provider

--- config/app.php
+++ config/app.php
@@ -193,6 +193,7 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
+        App\Providers\BindingServiceProvider::class,
         App\Providers\EventServiceProvider::class,        
         App\Providers\RouteServiceProvider::class,
         App\Providers\ViewServiceProvider::class,
