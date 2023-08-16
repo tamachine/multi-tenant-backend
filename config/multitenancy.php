215880226643d47cd1ce39c7b5a2b542f75738b5
tamar <tamar@TAMARA.local>
2023-08-16 10:54:41 +0200

    feature: add switchTenantConfigTask

--- config/multitenancy.php
+++ config/multitenancy.php
@@ -35,6 +35,7 @@ return [
     'switch_tenant_tasks' => [
         // \Spatie\Multitenancy\Tasks\PrefixCacheTask::class,
         \Spatie\Multitenancy\Tasks\SwitchTenantDatabaseTask::class,
+        App\Tasks\SwitchTenantConfigTask::class,
         // \Spatie\Multitenancy\Tasks\SwitchRouteCacheTask::class,
     ],
 
