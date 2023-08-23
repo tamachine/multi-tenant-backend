af35488a7293b2f0f84ba98a41672a2fe8a147bd
tamar <tamar@TAMARA.local>
2023-08-23 09:47:03 +0200

    feature: add images and locale to location endpoint

--- app/Http/Controllers/Api/LocationsController.php
+++ app/Http/Controllers/Api/LocationsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Api\BaseController;
 use Illuminate\Http\JsonResponse;
 use App\Models\Location;
+use Illuminate\Http\Request;
 
 class LocationsController extends BaseController
 {
@@ -12,9 +13,12 @@ class LocationsController extends BaseController
      /**
      * @lrd:start
      * ## Returns all locations ordered by appearance.
+     * @QAparam locale string nullable
      * @lrd:end           
      */
-    public function index():JsonResponse {        
+    public function index(Request $request):JsonResponse {       
+        $this->checkLocale($request);  
+
         return $this->successResponse($this->mapApiResponse(Location::orderBy('order_appearance')->get()));
     }
 
