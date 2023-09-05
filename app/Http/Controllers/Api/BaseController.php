e747dc343ec09860f7fbfe9a4cc49f160c13737c
tamar <tamar@TAMARA.local>
2023-09-05 11:38:29 +0200

    refactor: remove change key in Api/baseController

--- app/Http/Controllers/Api/BaseController.php
+++ app/Http/Controllers/Api/BaseController.php
@@ -55,13 +55,5 @@ abstract class BaseController extends Controller
 
     protected function addAttributesToApiResponse(object &$instance, array $attribute) {                
         $instance->setApiResponse(array_merge($instance->getApiResponse(), $attribute));
-    }
-
-    protected function changeKey(&$array, $originalKey, $newKey) {
-        if(isset($array[$originalKey])) {
-            $array[$newKey] = $array[$originalKey];
-
-            unset($array[$originalKey]);
-        }
-    }
+    }    
 }
