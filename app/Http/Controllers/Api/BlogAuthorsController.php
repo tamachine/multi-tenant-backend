df2c5ce57abc302e567280e4a7fa5991ac20e0c3
tamar <tamar@TAMARA.local>
2023-09-05 11:39:22 +0200

    feature: add seo configurations to blog author endpoint

--- app/Http/Controllers/Api/BlogAuthorsController.php
+++ app/Http/Controllers/Api/BlogAuthorsController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Api\BaseController;
 use App\Models\BlogAuthor;
+use App\Models\Page;
+use App\Traits\Controllers\Api\HasSeoConfigurations;
 use Illuminate\Http\JsonResponse;
 
 class BlogAuthorsController extends BaseController
 {
+    use HasSeoConfigurations;
 
      /**
      * @lrd:start
@@ -20,11 +23,18 @@ class BlogAuthorsController extends BaseController
 
         $this->checkLocale(request());
         
-        $response = $blogAuthor->toApiResponse();
-
-        $this->changeKey($response, 'getFeaturedImageModelImageInstance', 'photo');
-
-        return $this->successResponse($response);        
+        return $this->successResponse($blogAuthor->toApiResponse());        
     }  
        
+    /**
+     * @lrd:start
+     * ## Returns the seo configurations for a blogauthor
+     * ## blog_author_slug: slug of the author
+     * ## page_name: route_name of the page
+     * @QAparam locale string nullable 
+     * @lrd:end    
+     */
+    public function seoConfigurations(BlogAuthor $blogAuthor, Page $page):JsonResponse {
+        return $this->seoConfigurationsResponse($blogAuthor, $page);                
+    }
 }
