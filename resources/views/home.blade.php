399980cb101d898316571040aa7904b2cc49b368
Antonio Jenaro <antonio.jenaro@gmail.com>
2023-10-03 09:44:24 +0200

    landlord home new style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,89 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html>
+<html class="h-full" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Nave Homepage</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
 </head>
-<body>
-    <h2>Acceso paneles de administración de la Nave</h2>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, in ipsa. Adipisci at et, illo inventore iste minus, mollitia nobis odit placeat praesentium quae sunt. Cumque debitis error placeat possimus!
-    </p>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A autem deserunt dolor, ea eum id in non quo saepe voluptate. Asperiores consequuntur excepturi harum itaque minus quibusdam repellendus voluptates! Obcaecati?
-    </p>
-    <ul>
-        @foreach($tenants as $tenant)
-        <li><a href="{{ $tenant->login_url }}">{{ $tenant->domain }}</a></li>
-        @endforeach
-    </ul>
+<body class="h-full">
+
+<div class="min-h-full">
+    <nav class="border-b border-gray-200 bg-white">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div class="flex h-16 justify-between">
+                <div class="flex">
+                    <div class="flex flex-shrink-0 items-center">
+                        <img class="block h-8 w-auto lg:hidden" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+                        <img class="hidden h-8 w-auto lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+                    </div>
+                    <div class="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
+                        {{--<x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                            {{ __('Home') }}
+                        </x-nav-link>--}}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Mobile menu, show/hide based on menu state. -->
+        <div class="sm:hidden" id="mobile-menu">
+            <div class="space-y-1 pb-3 pt-2">
+               {{-- <x-responsive-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                    {{ __('Home') }}
+                </x-responsive-nav-link>--}}
+            </div>
+        </div>
+    </nav>
+
+    <div class="py-10">
+
+
+        <header>
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                Sites
+            </div>
+        </header>
+
+
+        <div class="mx-auto max-w-2xl sm:px-6 lg:px-8">
+            <!-- Your content -->
+            <div class="px-4 sm:px-6 lg:px-8">
+
+                <div class="mt-8 flow-root">
+                    <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div class="inline-block min-w-full py-2 align-middle">
+
+                            <table class="min-w-full divide-y divide-gray-300">
+                                <thead>
+                                <tr>
+                                    <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8">Name</th>
+                                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"></th>
+                                </tr>
+                                </thead>
+                                <tbody class="divide-y divide-gray-200 bg-white">
+                                @foreach($tenants as $tenant)
+                                    <tr>
+                                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">{{ $tenant->name }}</td>
+                                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                            <a href="{{ url($tenant->login_url) }}" class="rounded bg-indigo-600 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Login</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
 </body>
+
 </html>
