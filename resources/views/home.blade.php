8c914a5566c20e29a4834be672a7d97a03328176
Antonio Jenaro <antonio.jenaro@gmail.com>
2023-10-04 16:04:11 +0200

    replace tailwind css logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,8 +16,7 @@
             <div class="flex h-16 justify-between">
                 <div class="flex">
                     <div class="flex flex-shrink-0 items-center">
-                        <img class="block h-8 w-auto lg:hidden" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
-                        <img class="hidden h-8 w-auto lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+                        <img src="{{ asset('images/logo.jpeg') }}" alt="Logo" class="w-12">
                     </div>
                     <div class="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
                         {{--<x-nav-link :href="route('home')" :active="request()->routeIs('home')">
